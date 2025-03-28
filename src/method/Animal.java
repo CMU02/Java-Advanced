cd64bf3863da0fcd96749d013399fbd220d94974
HyeonJun <hyeonjun1968@naver.com>
2025-03-28 16:49:45 +0900

    ADD : Overloading & Overriding

---
+++ src/method/Animal.java
@@ -0,0 +1,37 @@
+package method;
+
+/**
+ * 부모 클래스의 메서드를 자식 클래스에서 재정의
+ * -> 동적 바인딩 (런타임에 결정됨)
+ * Point
+ * - speak()는 상위 클래스 Animal에서 정의가 됨
+ * - 하위 클래스가 `@Override`를 통해 동일한 메서드 이름/시그니처로 재정의]
+ * - 실제 객체 타입에 따라 메서드 동작이 달라짐
+ * - 동적 바인딩(Dynamic Binding)
+ */
+public class Animal {
+    public void speak() {
+        System.out.println("The animal makes a sound");
+    }
+
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.speak();
+
+        Animal cat = new Cat();
+        cat.speak();
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("멍멍!");
+    }
+}
+class Cat extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("야옹~");
+    }
+}
---
+++ src/method/Calculator.java
@@ -0,0 +1,35 @@
+package method;
+
+/**
+ * 같은 이름의 메서드를 다양한 매개변수 형태로 정의
+ * -> 정적 바인딩(컴파일 타임에 결정됨)
+ * Point
+ * - add 메서드 이름은 같지만, 매개변수 타입 및 개수가 다름
+ * - 컴파일러가 정확히 어던 메서드를 호출할지 미리 알 수 있음
+ * - 정적 바인딩(Static Binding)
+ */
+
+public class Calculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static int add(int ...arg) {
+        int sum = 0;
+        for (int i = 0; i < arg.length; i++) {
+            sum += arg[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(1, 2));
+        System.out.println(add(1.5, 2.5));
+        System.out.println(add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+}
