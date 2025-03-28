54cc1ce78bd822ce4879882127246098bb4537f0
HyeonJun <hyeonjun1968@naver.com>
2025-03-28 19:42:08 +0900

    ADD : HashSet & TreeSet

---
+++ src/Collection/Map/HashSetEx.java
@@ -0,0 +1,24 @@
+package Collection.Map;
+import java.util.*;
+
+/**
+ * 특징
+ * - 순서 없음
+ * - 중복 X
+ * - 탐색 빠름
+ */
+public class HashSetEx {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>();
+
+        names.add("철수");
+        names.add("영희");
+        names.add("철수"); // 중복 추가 시 무시
+
+        System.out.println("names.size() = " + names.size());
+
+        for (String name : names) {
+            System.out.println("name = " + name);
+        }
+    }
+}
---
+++ src/Collection/Map/TreeSetEx.java
@@ -0,0 +1,18 @@
+package Collection.Map;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetEx {
+    public static void main(String[] args) {
+        Set<Integer> scores = new TreeSet<>();
+
+        scores.add(70);
+        scores.add(100);
+        scores.add(85);
+        scores.add(92);
+        scores.add(65);
+
+        System.out.println("scores = " + scores); // 오름차순 정렬된 점수
+    }
+}
